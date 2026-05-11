55e040a30778657f9a773ce74d6c2cf6910df7c9
iegawa <fabianaiegawa@gmail.com>
2026-05-10 21:10:18 -0300

    Feat: add reminder service

---
+++ app/src/main/java/com/example/medtime/service/MedicationReminderService.kt
@@ -0,0 +1,43 @@
+package com.example.medtime.service
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.example.medtime.R
+
+class MedicationReminderService : Service() {
+
+    companion object {
+        const val CHANNEL_ID = "MedicationServiceChannel"
+        const val NOTIFICATION_ID = 1
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val notification = createNotification("MedTime está monitorando seus medicamentos")
+        startForeground(NOTIFICATION_ID, notification)
+        
+        // Aqui você carregaria os agendamentos do banco de dados e configuraria os alarmes
+        
+        return START_STICKY
+    }
+
+    private fun createNotification(contentText: String): Notification {
+        // TO DO
+    }
+
+    private fun createNotificationChannel() {
+        // TO DO
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+}
